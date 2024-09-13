542023de992f42b7fef5bb78e2017bfdc9f50207
Love Barrehag <lb224nj@student.lnu.se>
2024-09-13 11:28:24 +0200

    workshop part 1 done

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
-console.log("Hello World!");
-
-// kort basic function
-function divideNumbers(a, b) {
-  return a / b;
-}
-
-console.log(divideNumbers(12, 4));
-
-
-function multiplyNumbers(a, b) {
-  return a * b;
-}
-
-console.log(multiplyNumbers(12,4));
-
-
-// Let och const variabler
-let x = 16;
-x = 20;
-
-const y = 45
-
-console.log(x)
-console.log(y)
-
-// Kort basic class
-class student {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  showStudentInfo() {
-    console.log(`Name: ${this.name}, Age: ${this.age}`);
+const inputSentence = process.argv.slice(2)
+let longestWord = 'a'
+// Counting section
+for (const word of inputSentence) {
+  if (word.length >= longestWord.length) {
+    longestWord = word
   }
 }
+console.log(`longest word is ${longestWord}`)
+console.log('-----------------------------------------------------------------')
 
-const student1 = new student('Love', 28)
-student1.showStudentInfo();
+// Sorting section
+inputSentence.sort((a, b) => {
+  return b.length - a.length
+})
+console.log(inputSentence)
+console.log('-----------------------------------------------------------------')
+// console.log("\x1b[34m]", "Color text")
+words = inputSentence.length
+// for (let i = 0; i < words; i++) {
+  for (const word of inputSentence) {
+    for (const char of word) {
+      if (char === 'a' || char === 'o' || char === 'i' || char === 'e' || char === 'u' || char === 'y') {
+        console.log("\x1b[34m]", char)
+      } else {
+        console.log("\x1b[33m]", char)
+      }
+    }
+  }
+// }
